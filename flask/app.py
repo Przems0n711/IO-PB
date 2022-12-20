a42b730b429c2067415b838ec96e8faae2c42911
Rexoo <herricux99@gmail.com>
2022-12-20 16:31:25 +0100

    Update Repo
    Update 20/12/2022

---
+++ flask/app.py
@@ -0,0 +1,13 @@
+from flask import Response, Flask
+
+app = Flask(__name__)
+
+
+@app.post("/users")
+def add_user() -> Response:
+    print(request.json)
+    return Response(status=501)
+    
+
+
+
