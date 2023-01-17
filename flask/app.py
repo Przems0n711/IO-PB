c29cb515de1fb6c9e7efb57cb912002b3a337653
Rexoo <herricux99@gmail.com>
2023-01-17 16:09:36 +0100

    update app.py
    added get_user function

--- flask/app.py
+++ flask/app.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 def add_user() -> Response:
     print(request.json)
     return Response(status=501)
+
+@app.post("/users/")
+def get_user() -> Response:
+    print(request.json)
+    return Response(status=200)
