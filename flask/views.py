a6ab6500c75c0d7aa9866452f3ab25dfc71fb05d
Rexoo <herricux99@gmail.com>
2023-04-04 09:11:33 +0200

    update refactored controllers
    Updated refactored controllers with classes

--- flask/views.py
+++ flask/views.py
@@ -1,5 +1,44 @@
+from flask import jsonify, request
 from flask.views import MethodView
+from flask_classful import FlaskView, route
 
+class UserView(FlaskView):
+    def __init__(self):
+        self.users = UserRepository()
 
-class UserView(MethodView):
-    pass
+    def index(self):
+        abort(501)
+
+    def post(self):
+        user = request.json
+        self.users.add(user)
+        return jsonify(user), 201
+
+    def put(self, user_id):
+        user = self.users.get(user_id)
+        if not user:
+            abort(404)
+
+        user.update(request.json)
+
+        return jsonify(request.json), 200
+
+    @route('/<int:user_id>', methods=['PATCH'])
+    def partial_update(self, user_id):
+        user = self.users.get(user_id)
+        if not user:
+            abort(404)
+
+        key = list(request.json.keys())[0]
+        user[key] = request.json[key]
+
+        return jsonify(request.json), 200
+
+    def delete(self, user_id):
+        user = self.users.get(user_id)
+        if not user:
+            abort(404)
+
+        self.users.delete(user)
+
+        return '', 204
